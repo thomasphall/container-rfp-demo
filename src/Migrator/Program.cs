702ef2164bee87ebabc8876761c11d03b48eddad
Robert Sandoval <rsandova@redhat.com>
2019-09-07 14:43:50 -0500

    Update Program.cs

--- src/Migrator/Program.cs
+++ src/Migrator/Program.cs
@@ -35,7 +35,7 @@ namespace Migrator
                 : new SqlConnectionStringBuilder
                 {
                     ApplicationName = "RabbitMQ Stress Test Migrations",
-                    DataSource = "172.30.95.117",
+                    DataSource = "server.sandoval.svc.cluster.local",
                     InitialCatalog = "RabbitStressTest",
                     IntegratedSecurity = false,
                     MultipleActiveResultSets = false,
